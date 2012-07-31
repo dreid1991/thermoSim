5302cd5566ccbdc4e3d3e5942b8fdfa86893ad8c
Daniel Reid <dreid1991@gmail.com>
2012-07-31 16:00:07 -0700

    update

--- levels/Work.js
+++ levels/Work.js
@@ -59,7 +59,7 @@ _.extend(Work.prototype,
 		this.cutSceneStart("<p>Good afternoon!</p>"+
 		"Today we’re going to try to figure out why work changes a system’s temperature.  Let’s start with the equation for relating work to a temperature change:"+
 		"<p><center><img src='img/work/eq1.gif' alt='hoverhoverhover'></img></center></p>"+
-		"<p>This equation says that work is equal to how hard you compress a container times how much you compress it.  It also says that as you compress that container, the gas inside heats up.  But why does that happen?  What is it about pushing on a container that7 makes its molecules speed up?</p>"+
+		"<p>This equation says that work is equal to how hard you compress a container times how much you compress it.  It also says that as you compress that container, the gas inside heats up.  But why does that happen?  What is it about pushing on a container that makes its molecules speed up?</p>"+
 		"<p> One might say that it’s because energy is being added, and that is true, but we’re going to try to pin down the physical event that makes molecules speed up as a result of the container compressing.",
 		'intro'
 		);
