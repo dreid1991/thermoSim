599d2fca02932813a35fd78585b795d86bd2926f
Daniel Reid <dreid1991@gmail.com>
2012-09-13 14:07:34 -0700

    made seperate block for second heating in cvcp

--- levels/cvcp.js
+++ levels/cvcp.js
@@ -36,7 +36,7 @@ _.extend(cvcp.prototype,
 			},
 			{block:2,
 				title:"Current step",
-				text:"Okay, here’s a constant volume and a constant pressure container.  Both are adiabatic.  If you heat them both to a new temperature, How do the energies used compare?  ",
+				text:"Okay, here’s a constant volume and a constant pressure container.  Both are adiabatic.  If you heat them both to the same new temperature, How do the energies used compare?  ",
 				quiz: {	
 					type:'buttons',
 					options:
@@ -46,11 +46,11 @@ _.extend(cvcp.prototype,
 					]
 				},
 			},
-			{block:2,
+			{block:3,
 				title:"Current step",
 				text:"Good.  Now try heating them up again and try to figure out <i>why</i> the constant pressure system takes more energy to heat up.  By the way, work.",
 			},
-			{block:3,
+			{block:4,
 				cutScene: true,
 				text:"So what’s the verdict?",
 				quiz:{	
@@ -63,7 +63,7 @@ _.extend(cvcp.prototype,
 					]
 				},
 			},
-			{block:4,
+			{block:5,
 				cutScene:true,
 				text:"Good, so when we’re heating at constant volume, where does the energy go?",
 				quiz:{	
@@ -76,7 +76,7 @@ _.extend(cvcp.prototype,
 					]
 				},
 			},
-			{block:5,
+			{block:6,
 				cutScene: true,
 				text:"Yes, and we express this how?",
 				quiz:{
@@ -89,7 +89,7 @@ _.extend(cvcp.prototype,
 					]				
 				},
 			},
-			{block:6,
+			{block:7,
 				cutScene: true,
 				text:"<p>Yes.</p><p>It takes some amount of energy to heat up one mole these molecules one degree kelvin.  That amount is the heat capacity C<sub>V</sub>.  To get the total energy change in a temperature change, we multiply that amount by the change in temperature and the number of moles.  Thus we get<p>||EQ6CE<p>Okay, now what about in the constant pressure case?  Where does the energy go then?</p>",
 				quiz:{
@@ -102,7 +102,7 @@ _.extend(cvcp.prototype,
 					]				
 				},				
 			},
-			{block:7,
+			{block:8,
 				cutScene: true,
 				text:"So we’re putting energy into <i>two</i> things now!  So we know that </p>||EQ6aCE<p> is the energy that goes into heating the system.  How could we express the energy that goes into expanding the system again constant pressure?</p>",
 				quiz:{
@@ -115,11 +115,11 @@ _.extend(cvcp.prototype,
 					]				
 				},
 			},
-			{block:8,
+			{block:9,
 				cutScene: true,
 				text:"And if we put those two things together, we get...</p>||EQ11CE<p>OKAY.  We have our idea.  To heat under constant pressure, we have to expand the system, doing work.  This means that it will take more energy to heat something under constant pressure.   This means that C<sub>P</sub> is greater than C<sub>V</sub>.</p><p>Now watch this:</p><p>From the ideal gas law, we know ||EQ12CE Substituting in, we get ||EQ13CE  "
 			},
-			{block:9,
+			{block:10,
 				cutScene: true,
 				text: "||EQ13CENow we know that a heat capacity is a change in energy per temperate per amout of stuff whose temperature you're changing, so to get heat capacity from energy, we divide by nT.  This gives us ||EQ14CE So there we go!  Constant pressure heat capacity is R greater than constant volume heat capacity because of the work done when changing volume.</p><p>Neat things to to think about:<ul><li>We showed that C<sub>P</sub> was greater than C<sub>V</sub> for expanding.  Can you show that it is for compressing as well?  Better yet, draw where the energy goes.</li><li>In the previous page's first equation, we really just defined a change in enthalpy.  If a change in enthlpy is a change in internal energy plus some change in work energy, what is enthalpy?  Hint: Replace change with total.",
 			}
@@ -152,6 +152,9 @@ _.extend(cvcp.prototype,
 		walls[1].setDefaultReadout(this.readout);
 		walls[0].displayTemp().displayQ();
 		walls[1].displayTemp().displayQ();
+	},
+	block3Start: function(){
+		this.block2Start();
 	},
 
 
