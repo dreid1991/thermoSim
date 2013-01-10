4e63dbc8d0d1ea303d7e3b46d558f45542f1447d
Daniel Reid <dreid1991@gmail.com>
2013-01-10 14:02:13 -0800

    templating working

--- Templater.js
+++ Templater.js
@@ -7,13 +7,22 @@ Templater.prototype = {
 		obj.attrs = obj.attrs || {};
 		obj.style = obj.style || undefined;
 		obj.innerHTML = obj.innerHTML || '';
-		//maybe just make class attribute in attrs with [] by default
+		!obj.attrs.class ? obj.attrs.class = [globalHTMLClass] : obj.attrs.class.push(globalHTMLClass);
+		
 		return this.elemTemp(obj);
+	},
+	img: function(obj) {
+		obj.type = 'img';
+		return this.elem(obj);
+	},
+	div: function(obj) {
+		obj.type = 'div';
+		return this.elem(obj);
 	},
 	style: function(obj) {
 		return this.styleTemp(obj);
 	},
-	elemTemp: _.template("<<%= type %><% for (var attr in attrs) { %> <%= attr %>='<%= attrs[attr].join(' ') %>'<% } %> <%= templater.style({style: style}) %>><%= innerHTML %></<%= type %>>"),
-	styleTemp: _.template("<% if (!style) {return ''} %>style='<% for (var stylet in style) { %><%= stylet %>=<%= style[stylet] %>; <% } %>'")
+	elemTemp: _.template("<<%= type%><% for (var attr in attrs) { %><%= ' ' + attr %>='<%= attrs[attr].join(' ') %>'<% } %><%= style ? ' ' : '' %><%= templater.style({style: style}) %>><%= innerHTML %></<%= type %>>"),
+	styleTemp: _.template("<% if (!style) {return ''} %>style='<% for (var stylet in style) { %><%= stylet %>:<%= style[stylet] %>;<% } %>'")
 }
 
