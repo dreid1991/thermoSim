e0bed8f35a5b1986526d706430f8f0c2e6e0b60b
Daniel Reid <dreid1991@gmail.com>
2013-05-09 00:20:31 -0700

    added GNU license

--- Attractor.js
+++ Attractor.js
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2013  Daniel Reid
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 function Attractor() {
 	//energy = k1*e^(-k2*r)*r^2
 	//f = de/dr = k2*r*e^(-k2*r)*(2-k2*x)
--- Timeline.js
+++ Timeline.js
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2013  Daniel Reid
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 //need to interp all this stuff before rendering, yo
 function Timeline(parent, buttonManagerBlank, isSectionsBranch, isPromptsBranch) { //make it so it can inherit blanks as well
 	this.parent = parent;
