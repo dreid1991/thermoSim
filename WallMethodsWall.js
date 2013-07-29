685f4b8a079e54a98231d0f7cc07ab3968ef885c
Daniel Reid <dreid1991@gmail.com>
2013-07-29 13:42:27 -0700

    redundant calculation removed

--- WallMethodsWall.js
+++ WallMethodsWall.js
@@ -83,17 +83,16 @@ WallMethods.wall = {
 		// }
 		
 		
-		var dotVec = V(dot.x + dot.v.dx - perpUV.dx*dot.r - this[wallPtIdx].x, dot.y + dot.v.dy - perpUV.dy*dot.r - this[wallPtIdx].y);
+		var dotVec = new Vector(dot.x + dot.v.dx - perpUV.dx*dot.r - this[wallPtIdx].x, dot.y + dot.v.dy - perpUV.dy*dot.r - this[wallPtIdx].y);
 		var distPastWall = -perpUV.dotProd(dotVec);
 		var perpV = -perpUV.dotProd(dot.v);
-		if (distPastWall>=0 && distPastWall<=this.hitThreshold && this.isBetween(dot, wallPtIdx, wallUV, perpUV, distPastWall)){
+		if (distPastWall>=0 && distPastWall<=this.hitThreshold && this.isBetween(dot, wallPtIdx, wallUV, perpUV, perpV, distPastWall)){
 			this[this.hitMode](dot, wallPtIdx, wallUV, perpV, perpUV, distPastWall);
 			return true;
 		}
 		return false;
 	},
-	isBetween: function(dot, wallPtIdx, wallUV, perpUV, distPastWall){
-		var perpComponent = -dot.v.dotProd(perpUV); 
+	isBetween: function(dot, wallPtIdx, wallUV, perpUV, perpComponent, distPastWall){
 		var numVsPast = perpComponent == 0 ? 0 : distPastWall / perpComponent;
 		var dotPosAdj = new Point(dot.x - numVsPast * dot.v.dx, dot.y - numVsPast * dot.v.dy);
 		
